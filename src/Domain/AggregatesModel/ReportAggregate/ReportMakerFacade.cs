3be40705bc18f06da157bb73cbe9109af55c05b5
Illia Kotviskyi <iiiako197@gmail.com>
2024-05-05 15:02:43 +0300

    Implementing facade pattern

---
+++ src/Domain/AggregatesModel/ReportAggregate/ReportMakerFacade.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities.TransactionAggregate;
+using Domain.ValueObjects;
+using SharedKernel.Result;
+
+namespace Domain.AggregatesModel.ReportAggregate
+{
+    public sealed class ReportMakerFacade(CurrencyConversionService currencyConversionService, IReportService reportService)
+    {
+        private readonly CurrencyConversionService _currencyConversionService = currencyConversionService;
+        private readonly IReportService _reportService = reportService;
+
+        public async Task<Result<Report>> CreateReport(Currency preferredCurrency, IEnumerable<Transaction> transactions, DateRange dateRange)
+        {
+            var convertedTransactions = await this._currencyConversionService.ConvertTransactionsAsync(transactions, preferredCurrency);
+
+            return this._reportService.CreateReport(convertedTransactions);
+        }
+    }
+}

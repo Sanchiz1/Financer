0d8d2c2a25308ac21fd0a903c8203a0a4640cc9b
Sasha Zaitsev <sashazaitsev2005@gmail.com>
2024-05-06 19:02:33 +0300

    Created report builder, builder pattern

---
+++ src/Domain/AggregatesModel/ReportAggregate/Reports/Builder/IReportBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities.TransactionAggregate;
+using Domain.ValueObjects;
+
+namespace Domain.AggregatesModel.ReportAggregate.Reports.Builder;
+public interface IExpectsCurrency
+{
+    IExpectsSummary WithCurrency(Currency currency);
+}
+
+public interface IExpectsSummary
+{
+    IReportBuilder WithSummary(IEnumerable<Transaction> transactions);
+    IReportBuilder WithMonthlySummary(IEnumerable<Transaction> transactions);
+    IReportBuilder WithWeeklySummary(IEnumerable<Transaction> transactions);
+    IReportBuilder WithDailySummary(IEnumerable<Transaction> transactions);
+}
+
+public interface IReportBuilder
+{
+    Report Build();
+}
--- src/Domain/AggregatesModel/TransactionAggregate/ITransactionRepository.cs
+++ src/Domain/AggregatesModel/TransactionAggregate/ITransactionRepository.cs
@@ -6,6 +6,6 @@ namespace Domain.AggregatesModel.TransactionAggregate;
 public interface ITransactionRepository
 {
     Task<Transaction?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
-    Task<IReadOnlyList<Transaction>> GetUserTransactionsOfFundAsync(IUser user, DateRange dateRange, Guid fundId, CancellationToken cancellationToken = default);
-    Task<IReadOnlyList<Transaction>> GetUserTransactionsAsync(IUser user, DateRange dateRange, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<Transaction>> GetUserTransactionsOfFundAsync(DateRange dateRange, Guid fundId, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<Transaction>> GetUserTransactionsAsync(DateRange dateRange, CancellationToken cancellationToken = default);
 }

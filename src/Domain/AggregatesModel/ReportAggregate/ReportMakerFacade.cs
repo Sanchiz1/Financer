488db4b40a856228ba572a07a147c9b9c2a72781
Illia Kotviskyi <iiiako197@gmail.com>
2024-05-07 20:04:27 +0300

    Replaced IReportService usage with ICreateReportHandler

--- src/Domain/AggregatesModel/ReportAggregate/ReportMakerFacade.cs
+++ src/Domain/AggregatesModel/ReportAggregate/ReportMakerFacade.cs
@@ -1,20 +1,21 @@
 ﻿using Domain.AggregatesModel.ReportAggregate.Reports;
+using Domain.AggregatesModel.ReportAggregate.Reports.CreateReportHandler;
 using Domain.Entities.TransactionAggregate;
 using Domain.ValueObjects;
 using SharedKernel.Result;
 
 namespace Domain.AggregatesModel.ReportAggregate
 {
-    public sealed class ReportMakerFacade(CurrencyConversionService currencyConversionService, IReportService reportService)
+    public sealed class ReportMakerFacade(CurrencyConversionService currencyConversionService, ICreateReportHandler createReportHandler)
     {
         private readonly CurrencyConversionService _currencyConversionService = currencyConversionService;
-        private readonly IReportService _reportService = reportService;
+        private readonly ICreateReportHandler _createReportHandler = createReportHandler;
 
         public async Task<Result<Report>> CreateReport(Currency preferredCurrency, IEnumerable<Transaction> transactions, DateRange dateRange)
         {
             var convertedTransactions = await this._currencyConversionService.ConvertTransactionsAsync(transactions, preferredCurrency);
 
-            return this._reportService.CreateReport(convertedTransactions);
+            return this._createReportHandler.CreateReport(convertedTransactions);
         }
     }
 }
